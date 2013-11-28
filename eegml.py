ea7072f43c5893aa46a8913e2521309417697b6c
Sagar Jauhari <sagarjauhari@gmail.com>
2013-11-27 20:03:02 -0500

    Task_id of timestamps in raw file which cannot be found in task.xls is assigned as -1 now

--- eegml.py
+++ eegml.py
@@ -35,19 +35,19 @@ def format_time(ti):
     #Decimal not losing precision
     to = Decimal(to.strftime('%s.%f'))
     return str(to)
-    
+
 def format_task_xls(file):
     path_task_xls = join(config.DATA_URL, file + ".xls")
     path_task_xls_labels = join(config.SAVE_URL,  file + "_xls_labels.csv")
-    
+
     with open(path_task_xls, 'rb') as fi,\
     open(path_task_xls_labels, 'w') as fo:
         fr = csv.reader(fi, delimiter='\t')
         fw = csv.writer(fo, delimiter='\t')
-        
+
         h = fr.next()
         fw.writerow(['taskid',h[0], h[1], h[2], h[3], h[-1]]) #header
-        
+
         for idx, row in enumerate(fr):
             row[2] = format_time(row[2])
             row[3] = format_time(row[3])
@@ -64,13 +64,13 @@ def compress_time_labels(file_name):
         fr = csv.reader(fi, delimiter='\t')
         fw = csv.writer(fo, delimiter='\t')
         fw.writerow(fr.next()) #header
-        
+
         row1 = fr.next()
         t_id =row1[0]
         t_s = row1[1]
         t_e = row1[2]
         lab = row1[3]
-        
+
         for row in fr: #row 2 onwards
             if row[3] == lab:
                 t_e = row[2]
@@ -84,28 +84,28 @@ def compress_time_labels(file_name):
 
 def label_data(in_file, out_file, compressed_label_file, subj_t, time_t, dbg=False):
     if dbg: print "#"+subj_t + "--------"
-    
+
     with open(in_file, 'rb') as fi,\
     open(out_file, 'rb') as fi2,\
     open(compressed_label_file, 'w') as fo:
-        
+
         day = time_t[0:4]+"-"+time_t[4:6]+"-"+time_t[6:8]
         fr1 = csv.reader(fi,  delimiter=',') # combined.csv
         fr2 = csv.reader(fi2, delimiter='\t')# xls_labels.csv
         fw  = csv.writer(fo,  delimiter='\t')# combined_label_uncompress.csv
-        
+
         if dbg: print "day: " + day
-        
+
         #headers
         fw.writerow(next(fr1, None) + ['Difficulty', 'taskid'] )
         next(fr2, None)
-        
+
         #forward till subject data starts
         lab_row = fr2.next()
         while subj_t != lab_row[2]:
             lab_row = fr2.next()
         if dbg: print "start: " + str(lab_row[0])
-        
+
         for idx, row in enumerate(fr1):
             row[0] = datetime.strptime(day+' '+row[0]+'.0',\
                             '%Y-%m-%d %H:%M:%S.%f').strftime('%s.%f')
@@ -114,13 +114,13 @@ def label_data(in_file, out_file, compressed_label_file, subj_t, time_t, dbg=Fal
                 label = -1
                 fw.writerow(row + [label, lab_row[0]])
                 continue
-                
+
             if Decimal(row[0]) <= Decimal(lab_row[4]): # t <= end_time
                 if dbg: print str(idx)+": t <= end_time"
                 label = lab_row[5]
                 fw.writerow(row + [label, lab_row[0]])
                 continue
-            
+
             while Decimal(row[0] > lab_row[4]): # t > end_time
                 try:
                     lab_row = next(fr2)
@@ -132,7 +132,7 @@ def label_data(in_file, out_file, compressed_label_file, subj_t, time_t, dbg=Fal
                     if dbg: print e
                     break
             fw.writerow(row + [label,lab_row[0]])
-        
+
         if dbg: print "end:   "+str(lab_row[0])
     return
 
@@ -141,9 +141,9 @@ def create_raw_incremental(in_file, out_file):
     with open(in_file,'rb') as fi, open(out_file,'w') as fo:
         fr = csv.reader(fi, delimiter=',')
         fw = csv.writer(fo, delimiter=',')
-        
+
         fw.writerow(next(fr))#header
-        
+
         c=0.0
         prev_time = '100'#dummy
         for row in fr:
@@ -156,51 +156,55 @@ def create_raw_incremental(in_file, out_file):
 
 def label_data_raw(in_file, out_file, task_xls_label_file, mach_t, time_t, dbg=False):
     if dbg: print "#"+mach_t + "--------"
-    
+
     with open(in_file, 'rb') as fi,\
     open(task_xls_label_file, 'rb') as fi2,\
     open(out_file, 'w') as fo:
-        
+
         day = time_t[0:4]+"-"+time_t[4:6]+"-"+time_t[6:8]
         fr1 = csv.reader(fi,  delimiter=',') #microsec file
         fr2 = csv.reader(fi2, delimiter='\t')# xls_labels.csv
         fw  = csv.writer(fo,  delimiter='\t')
-        
+
         #headers
         fw.writerow(next(fr1, None) + ['Difficulty', 'taskid'] )
         next(fr2, None)
-        
+
         #forward till subject mach starts
         lab_row = fr2.next()
         while mach_t != lab_row[1]:
             lab_row = fr2.next()
         if dbg: print "start: " + str(lab_row[0])
-        
+
         for idx, row in enumerate(fr1):
             row[0] = datetime.strptime(day+' '+row[0],\
                             '%Y-%m-%d %H:%M:%S.%f').strftime('%s.%f')
             if Decimal(row[0]) < Decimal(lab_row[3]): # t < start_time
                 label = -1
-                fw.writerow(row + [label, lab_row[0]])
+                taskid= -1
+                fw.writerow(row + [label, taskid])
                 continue
-                
+
             if Decimal(row[0]) < Decimal(lab_row[4]): # t < end_time
                 label = lab_row[5]
-                fw.writerow(row + [label, lab_row[0]])
+                taskid= lab_row[0]
+                fw.writerow(row + [label, taskid])
                 continue
-            
+
             while Decimal(row[0] > lab_row[4]): # t > end_time
                 try:
                     lab_row = next(fr2)
                     label = lab_row[5]
+                    taskid=lab_row[0]
                 except: # reached end of file
                     label = -1
+                    taskid= -1
                     break
-            fw.writerow(row + [label, lab_row[0]])
-        
+            fw.writerow(row + [label, taskid])
+
         if dbg: print "end:   "+str(lab_row[0])
     return
-    
+
 def plot_signal(x_ax, y_ax, label, ax=None):
     if ax==None:
         fig, ax = plt.subplots()
@@ -210,7 +214,7 @@ def plot_signal(x_ax, y_ax, label, ax=None):
     plt.legend(loc='upper left')
     plt.show()
     return ax
-    
+
 def butter_bandpass(lowcut, highcut, fs, order=5):
     """
     http://wiki.scipy.org/Cookbook/ButterworthBandpass
@@ -225,7 +229,7 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
     y = lfilter(b, a, data)
     return y
-    
+
 def plot_butter(fs, lowcut, highcut, orders):
     """Plot the frequency response for a few different orders."""
     plt.figure()
@@ -248,21 +252,21 @@ def do_filter_signal(data, low_cut, high_cut, fs, order, out_file):
                                            fs,
                                            order)
     limit=2000
-    
+
     fig, ax = plt.subplots()
     ax.plot(data[0:limit], label="Original Signal")
     ax.plot(data_filtered[0:limit], label="Filtered Signal")
     plt.grid(True)
     plt.legend(loc='best')
     plt.title("data[0:"+str(limit)+"]")
-    
+
     if out_file is not None:
         with open(out_file,'w') as fo:
             fw = csv.writer(fo)
             fw.writerow(list(data_filtered))
-    
+
     return data_filtered
-        
+
 def get_subject_list(dir_url):
     onlyfiles = [ f for f in listdir(dir_url) if isfile(join(dir_url,f)) ]
     pat = re.compile("[0-9]*\.[0-9]*\.labelled\.csv")
@@ -284,11 +288,11 @@ def get_data(subj_list, dir_url):
 
 def get_counts(data):
     return [(i,len(data[i])) for i in data]
-    
+
 def plot_subject(s_comb, title=None):
     fig, ax = plt.subplots()
     x_ax = [int(i[0].split('.')[0]) for i in s_comb]
-    
+
     sig_q = [int(i[1]) for i in s_comb]
     atten = [int(i[2]) for i in s_comb]
     medit = [int(i[3]) for i in s_comb]
@@ -296,13 +300,13 @@ def plot_subject(s_comb, title=None):
     taskid= [int(i[5]) for i in s_comb]
     taskid_set = list(set(taskid))
     taskid_norm = [taskid_set.index(i) for i in taskid]
-    
+
     ax.plot(x_ax,sig_q, label='Quality')
     ax.plot(x_ax, atten, label='Attention')
     ax.plot(x_ax, medit, label='Meditation')
     ax.plot(x_ax, diffi, label='Difficulty')
     #ax.plot(x_ax, taskid_norm, label='taskid')
-    
+
     ax.grid(True)
     fig.tight_layout()
     plt.legend(loc='upper left')
@@ -332,9 +336,9 @@ def clean_all(subj_list, subj_data):
 def plot_cleaned_counts(subj_data, cln_data):
     cnt1=get_counts(subj_data)
     cnt2=get_counts(cln_data)
-    
+
     fig, ax = plt.subplots()
-    
+
     ax.plot([i[0] for i in cnt1], [i[1] for i in cnt1], "-o", label="original")
     ax.plot([i[0] for i in cnt2], [i[1] for i in cnt2], "-o", label="cleaned")
     plt.xlabel("Id")
@@ -342,17 +346,17 @@ def plot_cleaned_counts(subj_data, cln_data):
     plt.legend()
     plt.grid()
     plt.title("Comparing original and new size of data")
-    
+
 def get_num_words(DATA_URL):
     path_task_xls = DATA_URL + "/task.xls"
-    
+
     with open(path_task_xls, 'rb') as fi:
         fr = csv.reader(fi, delimiter='\t')
         next(fr)#header
 
         data = list(fr)
         data_cols = zip(*data)
-        
+
         l=len(data_cols[0])
         num_words_stim = [float(len(i.split())) for i in data_cols[4]]
         num_chars_stim = [float(len(i)) for i in data_cols[4]]
@@ -360,23 +364,23 @@ def get_num_words(DATA_URL):
         time_diff = [float(Decimal(format_time(data_cols[3][i]))-\
                     Decimal(format_time(data_cols[2][i])))\
                     for i in xrange(l)]
-        
+
         time_per_word = [time_diff[i]/num_words_stim[i] for i in range(l)]
         time_per_char = [time_diff[i]/num_chars_stim[i] for i in range(l)]
-        
+
         sentence_idx=[i for i in xrange(l) if num_words_stim[i] > 1]
-        
+
         print pearsonr(time_per_word, difficulty)
         print pearsonr(time_per_char, difficulty)
 
-        print pearsonr([time_per_word[i] for i in sentence_idx], 
+        print pearsonr([time_per_word[i] for i in sentence_idx],
                        [difficulty[i] for i in sentence_idx])
-        print pearsonr([time_per_char[i] for i in sentence_idx], 
+        print pearsonr([time_per_char[i] for i in sentence_idx],
                        [difficulty[i] for i in sentence_idx])
 
         tpa = [difficulty[i] for i in sentence_idx]
         plt.hist(tpa)
-        
+
 # Create dict of machine data
 def create_dict_machine_data(raw_dir):
     onlyfiles_raw = [ f for f in listdir(raw_dir) if isfile(join(raw_dir,f)) ]
@@ -384,21 +388,21 @@ def create_dict_machine_data(raw_dir):
     temp_dat_raw = [f.split('.')[0:2] for f in onlyfiles_raw if pat_raw.match(f)]
     mach_dict = {i[1]: i[0] for i in temp_dat_raw}
     return mach_dict
-    
+
 def get_performance(x,y):
-    """ Measures the performance metrics for x(actual) 
+    """ Measures the performance metrics for x(actual)
         and y (experimental).
     """
     if len(x) != len(y):
         print "Error: Lengths not same"
         return
     TP = FN = FP = TN = 0.0
-    
+
     for i in range(0,len(x)):
         for j in range(0, len(x)):
             if i == j:
                 continue
-            
+
             if x[i]==x[j] and y[i]==y[j]:
                 TP = TP + 1
             elif x[i]!=x[j] and y[i]!=y[j]:
@@ -411,12 +415,12 @@ def get_performance(x,y):
     TN = TN/2
     FN = FN/2
     FP = FP/2
-    
+
     accuracy = (TP + TN) / (TP + TN + FP + FN)
     precision = TP/(TP + FP)
     recall = TP/(TP + FN)
     fscore = 2*precision*recall/(precision + recall)
-    
+
     print "  Accuracy: \t" + str(round(accuracy, 3))
     print "  Precision: \t" + str(round(precision, 3))
     print "  Recall: \t" + str(round(recall, 3))
