d8b8f4f0b082c1d0d9dc607f71dc5a1e3997c806
Justis Peters <justis@mindshareit.com>
2013-11-29 16:14:49 -0500

    take filenames as command-line parameters and handle delimiters, since CMU is sending us both comma separated and tab separated files

--- labels_from_text.py
+++ labels_from_text.py
@@ -1,16 +1,21 @@
 import csv
 import os
+import sys
 
-def make_passage_list (file, col):
+def make_passage_list (file, col, delimiter="\t"):
+	"Given a file and a column, extract column as a sorted list"
+	# TODO deduplicate, probably using a set
+	# TODO trim whitespace
 	passage_list = []
 	with open(file, 'rb') as inFile:
-		fi = csv.reader(inFile)
+		fi = csv.reader(inFile, delimiter=delimiter)
 		for row in fi:
 			passage_list.append(row[col])
 	passage_list.sort()
 	return passage_list
 
 def assign_id (passage_list):
+	"Assign a unique ID to every item in a list"
 	passage_id = {}
 	index = 0;
 	for item in passage_list:
@@ -21,10 +26,10 @@ def assign_id (passage_list):
 	return passage_id
 
 
-def word_data(file, col1, col2):
+def word_data(file, col1, col2, delimiter=','):
 	word_info = {}
 	with open(file, 'rb') as reader:
-		fi = csv.reader(reader)
+		fi = csv.reader(reader, delimiter=delimiter)
 		for row in fi:
 			key = row[col1]
 			if key in word_info:
@@ -49,41 +54,29 @@ def passage_data(passage_id, word_info):
 		passage_info[v] = string_data(word_info, k)
 	return passage_info
 
-def get_dict(passage_id, file, col1, col2):
-	word_info = word_data(file, col1, col2)
+def get_dict(passage_id, file, col1, col2, delimiter=','):
+	word_info = word_data(file, col1, col2, delimiter=delimiter)
 	passage_info = passage_data(passage_id, word_info)
 	return passage_info
 
 
+if __name__ == "__main__":
+	if len(sys.argv) != 4:
+		print "usage: %s [aoa.csv] [tasks.csv] [out.csv]" % (sys.argv[0])
+		sys.exit()
 
-passage_file = r"C:\Users\Vinaya\Desktop\test.csv"
-passage_col =  4
-passage_dif = 9
-passage_list = make_passage_list(passage_file, passage_col)
-passage_id = assign_id(passage_list)
+	aoa_file, passage_file, output_file = sys.argv[1:]
 
-passage_diff = get_dict(passage_id, passage_file, passage_col, passage_dif)
+	passage_col =  4
+	passage_list = make_passage_list(passage_file, passage_col)
+	passage_id = assign_id(passage_list)
 
-aoa_file = r"C:\Users\Vinaya\Desktop\aoa-data.csv"
-aoa_word_col = 0
-aoa_col = 10
-aoa_dict = get_dict(passage_id, aoa_file, aoa_word_col, aoa_col)
+	passage_diff = get_dict(passage_id, passage_file, passage_col, 9, delimiter='\t')
+	aoa_dict = get_dict(passage_id, aoa_file, 0, 10)
+	frequency_dict = get_dict(passage_id, aoa_file, 0, 2)
 
-frequency_file = r"C:\Users\Vinaya\Desktop\frequency.csv"
-frequency_word_col = 1
-frequency_col =0
-frequency_dict = get_dict(passage_id, frequency_file, frequency_word_col, frequency_col)
-
-output_file = r"C:\Users\Vinaya\Desktop\classifiers.csv"
-with open (output_file, 'w+') as output:
-	out = csv.writer(output)
-	out.writerow(["id", "passage", "difficulty", "aoa", "frequency"])
-	for k, v in passage_id.iteritems():
-		out.writerow([v, k, passage_diff[v], aoa_dict[v], frequency_dict[v]])
-
-#print "done"
-
-#for k, v in passage_id.iteritems():
-		#print v, k, passage_difficulty[v], aoa_dict[v], frequency_dict[v]
-
-
+	with open (output_file, 'w+') as output:
+		out = csv.writer(output)
+		out.writerow(["id", "passage", "difficulty", "aoa", "frequency"])
+		for k, v in passage_id.iteritems():
+			out.writerow([v, k, passage_diff[v], aoa_dict[v], frequency_dict[v]])
