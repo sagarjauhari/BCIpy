768379b61f6ff64b910d2e1a90353a4b240f2087
Justis Peters <justis@mindshareit.com>
2013-11-30 23:34:45 -0500

    first draft of Slicer

---
+++ slicer.py
@@ -0,0 +1,59 @@
+import pandas as pd, numpy as np
+import sys
+import rolling_windows
+
+class Slicer(object):
+    def __init__(self, taskfile='data/task.xls'):
+        self.load_tasks_from_tsv(taskfile)
+        self.series = {}
+
+    def load_tasks_from_tsv(self, taskfile):
+        "reads task data from tab delimited file"
+        self.tasks = pd.read_table('data/task.xls',
+            parse_dates=['start_time', 'end_time'], index_col=False)
+
+    def load_series_from_csv(self, seriesname, csvfile):
+        self.series[seriesname] = pd.read_csv(csvfile, parse_dates=[0], index_col=0, squeeze=True)
+
+    def load_series_from_pickle(self, seriesname, picklefile):
+        self.series[seriesname] = pd.read_pickle(picklefile)
+
+    def get_by_task_id(self, taskid, features=[]):
+        task = self.tasks.loc[taskid]
+        st, et = task['start_time':'end_time']
+
+        task = task.to_dict()
+        task.update({f:self.series[f].between_time(st,et) for f in features})
+        return task
+
+    def print_series_info(self):
+        print ["%s: %s" % (k, type(s)) for k,s in self.series.iteritems()]
+
+    def extract_rolling_median(self, seriesname='raw', window_size=128):
+        new_feature_name = seriesname+'_rolling_median_'+str(window_size)
+        self.series[new_feature_name]=rolling_windows.downsampled_rolling_median(
+            self.series[seriesname],
+            window_size=window_size
+        )
+        self.print_series_info()
+
+    def extract_rolling_PSD(self, seriesname='raw', window_size=512):
+        new_feature_name = seriesname+'_rolling_PSD_'+str(window_size)
+        self.series[new_feature_name]=rolling_windows.rolling_power_ratio(
+            self.series[seriesname],
+            window_size=window_size
+        )
+        self.print_series_info()
+
+
+if __name__ == '__main__':
+    s = Slicer()
+    print 'loading raw from pickle'
+    s.load_series_from_pickle('raw', sys.argv[1])
+    s.print_series_info()
+    print 'extracting rolling median'
+    s.extract_rolling_median()
+    print 'extracting rolling PSD'
+    s.extract_rolling_PSD()
+    print 'fetching task 1, with features'
+    print s.get_by_task_id(1, features=['raw','raw_rolling_PSD_512', 'raw_rolling_median_128'])
