6e2a9307d69fd3d01d3a093edc6a942e48319ae4
Justis Peters <justis@mindshareit.com>
2013-12-01 00:55:00 -0500

    now you can import raw signal from a list of csvfiles

--- slicer.py
+++ slicer.py
@@ -13,7 +13,11 @@ class Slicer(object):
             parse_dates=['start_time', 'end_time'], index_col=False)
 
     def load_series_from_csv(self, seriesname, csvfile):
-        self.series[seriesname] = pd.read_csv(csvfile, parse_dates=[0], index_col=0, squeeze=True)
+        try: # assume a list of files
+            self.series[seriesname] = pd.concat([pd.read_csv(filename, parse_dates=[0], index_col=0,
+                squeeze=True) for filename in csvfile]).sort_index()
+        except TypeError:
+            self.series[seriesname] = pd.read_csv(csvfile, parse_dates=[0], index_col=0, squeeze=True)
 
     def load_series_from_pickle(self, seriesname, picklefile):
         self.series[seriesname] = pd.read_pickle(picklefile)
@@ -35,7 +39,6 @@ class Slicer(object):
             self.series[seriesname],
             window_size=window_size
         )
-        self.print_series_info()
 
     def extract_rolling_PSD(self, seriesname='raw', window_size=512):
         new_feature_name = seriesname+'_rolling_PSD_'+str(window_size)
@@ -43,13 +46,18 @@ class Slicer(object):
             self.series[seriesname],
             window_size=window_size
         )
-        self.print_series_info()
-
 
 if __name__ == '__main__':
+    print 'instantiating task slicer'
     s = Slicer()
-    print 'loading raw from pickle'
-    s.load_series_from_pickle('raw', sys.argv[1])
+
+    if len(sys.argv) > 2:
+        print 'loading raw from list of csvfiles'
+        s.load_series_from_csv('raw', sys.argv[1:])
+    else:
+        print 'loading raw from pickle'
+        s.load_series_from_pickle('raw', sys.argv[1])
+
     s.print_series_info()
     print 'extracting rolling median'
     s.extract_rolling_median()
