2d148c56793f289f3f9e1951da44af7233c9f8d6
Sagar Jauhari <sagarjauhari@gmail.com>
2014-02-18 22:34:16 -0500

    added chartsforpaper option, slicer object now creating in this file

--- process_eeg.py
+++ process_eeg.py
@@ -7,8 +7,9 @@ Created on Tue Feb 18 00:09:18 2014
 
 import argparse, os, datetime, shutil, glob
 from os.path import join, isfile
-
-import process_series_files, kernel_svm
+from slicer import Slicer
+import process_series_files, kernel_svm, charts_for_paper
+from matplotlib.backends.backend_pdf import PdfPages
 
 def arg_parse():
     parser = argparse.ArgumentParser(description='Process EEG Data.')
@@ -17,8 +18,9 @@ def arg_parse():
                         help='Directory containing raw EEG Files.')
 
     parser.add_argument('-o', nargs=1, required=True, dest='outdir',
-                        help='Path of processed data and reports. New directory\
-                        with timestamp will be created inside this folder.')
+                        help='Path of processed data and reports. New \
+                        directory with timestamp will be created inside this\
+                        folder.')
 
     parser.add_argument('--interpolate', action='store_true',
                         help='Interpolate 512Hz time stamps on 512Hz data\
@@ -26,6 +28,9 @@ def arg_parse():
 
     parser.add_argument('--kernelsvm', action='store_true',
                         help='Perform Kernelized SVC on interpolated raw data')
+                        
+    parser.add_argument('--chartsforpaper', action='store_true',
+                        help='Create charts in single page PDF files.')
                         
     args = parser.parse_args()
     return args
@@ -36,6 +41,7 @@ def create_timestamp_dir(out_dir):
     try:
         os.makedirs(mydir)
         os.makedirs(join(mydir,'data'))
+        os.makedirs(join(mydir,'reports'))
     except OSError, e:
         if e.errno != 17:
             raise # This was not a "directory exist" error
@@ -45,16 +51,29 @@ def create_timestamp_dir(out_dir):
 if __name__=="__main__":
     args = arg_parse()
     out_dir = create_timestamp_dir(args.outdir[0])
-    data_dir= join(out_dir,'data')
+    data_dir= args.indir[0]
+    report_dir = join(out_dir, 'reports')
 
     if args.interpolate:
         process_series_files.process_all_in_dir(args.indir[0],
                                                 join(out_dir,'data'))
+        data_dir = join(out_dir,'data')
+        
+    """
     else: #just copy the files
         print "Copying data files to ", data_dir
         for csvf in glob.iglob(join(args.indir[0],"*.csv")):
             shutil.copyfile(csvf, join(data_dir, os.path.basename(csvf)))
-
+    """
+    print "Instantiating Slicer and loading series"
+    slicer = Slicer()
     filelist=[join(data_dir,f) for f in os.listdir(data_dir)]
+    slicer.load_series_from_csv('raw', filelist)
+    
     if args.kernelsvm:
-        kernel_svm.do_kernelsvm_slicer(filelist)
+        kernel_svm.do_kernelsvm_slicer(slicer)
+    
+    if args.chartsforpaper:
+        pp = PdfPages(join(report_dir, 'rolling_median.pdf'))
+        charts_for_paper.do_charts(slicer, pp)
+        pp.close()
