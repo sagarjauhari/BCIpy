771f59a7503f17f180a26fd5e5c3132e32e6051c
Justis Peters <jgpeter2@ncsu.edu>
2014-02-24 14:48:10 -0500

    docstrings for Slicer.py

--- slicer.py
+++ slicer.py
@@ -12,6 +12,8 @@ except ImportError:
     pass
 
 class Slicer(object):
+    """Loads data and metadata, extracts important features, and provides methods to retrieve subsets"""
+
     def __init__(self, taskfile=join(ALL_RAW_URL, 'task.xls')):
         self.load_tasks_from_tsv(taskfile)
         self.series = {}
@@ -20,13 +22,14 @@ class Slicer(object):
         return self.tasks
 
     def load_tasks_from_tsv(self, taskfile):
-        "reads task data from tab delimited file"
+        """Reads task data from tab delimited file"""
         t = pd.read_table(taskfile, parse_dates=['start_time', 'end_time'], index_col=False)
         t['word_count'] = t.stim.apply(lambda x: len(x.split()))
         t['is_passage'] = t.word_count.apply(lambda x: x > 1)
         self.tasks = t
 
     def load_series_from_csv(self, seriesname, csvfilelist):
+        """Reads a single series from a CSV file and merges it with the current data"""
         self.series[seriesname] = pd.concat([
             pd.read_csv(filename, parse_dates=[0], index_col=0,
                 squeeze=True).tz_localize(pytz.UTC).tz_convert(pytz.timezone('US/Eastern'))
@@ -34,14 +37,17 @@ class Slicer(object):
         ]).sort_index()
 
     def load_series_from_pickle(self, seriesname, picklefile):
+        """For quick restore from previous Slicer state"""
         self.series[seriesname] = pd.read_pickle(picklefile)
 
     def get_passage_tasks_by_difficulty(self, difficulty, features=[]):
+        """Gets data for only the tasks which have the specified difficulty, along with any features specified in the 'features' arg"""
         t = self.tasks
         taskids = t[t.difficulty==difficulty][t.is_passage].index
         return [self.get_by_task_id(taskid, features=features) for taskid in taskids]
 
     def get_by_task_id(self, taskid, features=[]):
+        """Get just one task, by taskid, with the features specified in the 'features' arg"""
         task = self.tasks.loc[taskid]
         st, et = task['start_time':'end_time']
         st = st.tz_localize(pytz.timezone('US/Eastern'))
@@ -52,6 +58,7 @@ class Slicer(object):
         return task
 
     def extract_first_n_median(self, n=10):
+        """Extracts just the first n samples from the rolling median, primarily to normalize sample vectors to the same length."""
         X = [
             self.get_n_samples_by_taskid(taskid, 'raw_rolling_median_128', n)
             for taskid in self.tasks.index
@@ -59,6 +66,7 @@ class Slicer(object):
         self.tasks = self.tasks.combine_first(pd.DataFrame(X, index=self.tasks.index))
 
     def get_n_samples_by_taskid(self, taskid, feature, n=10):
+        """For the specified taskid, return just the first n samples"""
         task = self.tasks.loc[taskid]
         st, et = task['start_time':'end_time']
         st = st.tz_localize(pytz.timezone('US/Eastern'))
@@ -69,9 +77,11 @@ class Slicer(object):
         return ret
 
     def print_series_info(self):
+        """Prints info about all series available, primarily for debugging purposes"""
         print ["%s: %s" % (k, type(s)) for k,s in self.series.iteritems()]
 
     def extract_rolling_median(self, seriesname='raw', window_size=128):
+        """Extracts a rolling median for the specified series"""
         new_feature_name = seriesname+'_rolling_median_'+str(window_size)
         self.series[new_feature_name]=rolling_windows.downsampled_rolling_median(
             self.series[seriesname],
@@ -79,6 +89,7 @@ class Slicer(object):
         )
 
     def extract_rolling_PSD(self, seriesname='raw', window_size=512):
+        """Extracts power spectral density (PSD) for the specified series"""
         new_feature_name = seriesname+'_rolling_PSD_'+str(window_size)
         self.series[new_feature_name]=rolling_windows.rolling_power_ratio(
             self.series[seriesname],
@@ -86,6 +97,7 @@ class Slicer(object):
         )
 
     def extract_filtered_signal(self, seriesname='raw', fs=512.0, lowcut=0.1, highcut=20.0):
+        """Applies a Butterworth bandpass filter to the specified series"""
         self.series[seriesname+'_butter_filtered'] = filters.butter_bandpass_filter(
             self.series[seriesname],
             lowcut=lowcut,
@@ -93,6 +105,7 @@ class Slicer(object):
             fs=fs,
             order=4)
 
+# If run from the command line, do some basic tests and ouput some debugging info
 if __name__ == '__main__':
     print 'instantiating task slicer'
     s = Slicer()
